4ba9377fa96e157d4a19ed710823abe34f8feb91
vinicius <vinilavor@hotmail.com>
2018-09-01 12:36:51 -0300

    initial commit

---
+++ agenda/app/Http/Controllers/AgendaController.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+
+class AgendaController extends Controller
+{
+    public function salvar(Request $request)
+    {
+
+    }
+
+    public function cagar(Request $request)
+    {
+
+    }
+    public function nada(Request $request)
+    {
+
+    }
+
+
+}
